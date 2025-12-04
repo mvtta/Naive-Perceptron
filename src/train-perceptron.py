c7f27433b8ad59f226fe4dcc021377c869e205d3
Mariana Valdetaro <70669920+mvtta@users.noreply.github.com>
2025-12-04 00:01:35 +0000

    So simple
    Refactor code for clarity and organization.

--- src/train-perceptron.py
+++ src/train-perceptron.py
@@ -1,37 +1,26 @@
-# import libs
-# load dataset
-# create traininf and test split
-# instantiate costum perceptron
-# fit the model
-# score the model
-
-from perceptron import *
 import numpy as np
 import pandas as pd
+
+from perceptron import *
+
 from sklearn.datasets import load_iris
-from sklearn.preprocessing import StandardScaler, KBinsDiscretizer
 from sklearn.compose import ColumnTransformer
 from sklearn.model_selection import train_test_split
-
-
+from sklearn.preprocessing import StandardScaler, KBinsDiscretizer
 
 
 # Load the data set
-#
 X, y = load_iris(as_frame=True, return_X_y=True)
 
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.3, random_state=42, stratify=y)
-#
-# Instantiate CustomPerceptron
-#
+X_train, X_test, y_train, y_test = train_test_split(X, y, 
+                                                    test_size=0.3, 
+                                                    random_state=42, 
+                                                    stratify=y)
+# Instantiate
 prcptrn = CustomPerceptron()
 
-# Fit the model
-#
+# Fit
 prcptrn.fit(X_train, y_train)
 
-
-# Score the model
-#
+# Score
 prcptrn.score(X_test, y_test), prcptrn.score(X_train, y_train)
